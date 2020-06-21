e0a64b442931842852aefa277914125a29cc9adb
John Baylor <john.baylor@gmail.com>
2020-06-21 14:00:27 -0700

    Add basic list of pizzas to front pages.

--- lib/todays_pizza_web/controllers/page_controller.ex
+++ lib/todays_pizza_web/controllers/page_controller.ex
@@ -1,7 +1,37 @@
 defmodule TodaysPizzaWeb.PageController do
   use TodaysPizzaWeb, :controller
 
+  def cheeseboard_url do
+    "https://cheeseboardcollective.coop/pizza/"
+  end
+
+  def extract_dates_and_toppings([date, pizza]) do
+    # Expected format:
+    #[
+    #  {"p", [], ["Wed Jun 24"]},
+    #  {"p", [],
+    #    [
+    #      "***This week we are offering partially baked pizzas to finish cooking at home. There are limited vegan, gluten-free friendly, and vegan+gluten-free friendly pizzas available also***",
+    #      {"br", [], []},
+    #      {"br", [], []},
+    #      "Artichoke, kalamata olive, fresh ricotta made in Berkeley by Belfiore, mozzarella cheese, and house made tomato sauce"
+    #    ]
+    #  }
+    #]
+    # => ["Wed Jun 24", "Artichoke, kalamata olive, ..."]
+    [
+      elem(date, 2),
+      elem(pizza, 2) |> List.last
+    ]
+  end
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    html = HTTPoison.get!( cheeseboard_url() ).body
+    {:ok, document} = Floki.parse_document(html)
+    pizza_days = Floki.find(document, ".pizza-list") |> Floki.find("article") |> Floki.find("p")
+    dates_and_toppings = Enum.chunk_every(pizza_days, 2, 2)
+                         |> Enum.map( &extract_dates_and_toppings(&1) )
+
+    render(conn, "index.html", %{dates_and_toppings: dates_and_toppings})
   end
 end
--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule TodaysPizza.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:floki, "~> 0.26.0"},
+      {:httpoison, "~> 1.6"}
     ]
   end
 
